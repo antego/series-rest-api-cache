cf9699c7534d1b27d7c2c9ec2a249ed42c09fd98
antego <antego@users.noreply.github.com>
2017-06-15 23:16:25 +0300

    Revert: Merge .gitignore

--- src/main/java/org/n52/cache/CachingDataService.java
+++ src/main/java/org/n52/cache/CachingDataService.java
@@ -29,25 +29,66 @@
 
 package org.n52.cache;
 
+import org.n52.io.request.IoParameters;
 import org.n52.io.request.RequestParameterSet;
+import org.n52.io.response.OutputCollection;
+import org.n52.io.response.ParameterOutput;
 import org.n52.io.response.dataset.AbstractValue;
 import org.n52.io.response.dataset.Data;
 import org.n52.io.response.dataset.DataCollection;
 import org.n52.series.spi.srv.DataService;
+import org.n52.series.spi.srv.ParameterService;
 import org.n52.series.spi.srv.RawDataService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * TODO: JavaDoc
  *
  */
-public class CachingDataService implements DataService<Data<AbstractValue< ? >>> {
+public class CachingDataService extends ParameterService<ParameterOutput>
+        implements DataService<Data<AbstractValue< ? >>> {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CachingDataService.class);
 
-    @Override
-    public DataCollection<Data<AbstractValue< ? >>> getData(RequestParameterSet parameters) {
-        return null;
+    private ParameterService<ParameterOutput> parameterService;
+
+    private DataService<Data<AbstractValue< ? >>> dataService;
+
+    private DataCache cache;
+
+    public CachingDataService(ParameterService<ParameterOutput> parameterService,
+                              DataService<Data<AbstractValue< ? >>> dataService,
+                              DataCache cache) {
+        this.parameterService = parameterService;
+        this.dataService = dataService;
+        this.cache = cache;
     }
 
+    @Override
+    public OutputCollection<ParameterOutput> getExpandedParameters(IoParameters query) {
+        return parameterService.getExpandedParameters(query);
+    }
+
+    @Override
+    public OutputCollection<ParameterOutput> getCondensedParameters(IoParameters query) {
+        return parameterService.getCondensedParameters(query);
+    }
+
+    @Override
+    public OutputCollection<ParameterOutput> getParameters(String[] items, IoParameters query) {
+        return parameterService.getParameters(items, query);
+    }
+
+    @Override
+    public ParameterOutput getParameter(String item, IoParameters query) {
+        return parameterService.getParameter(item, query);
+    }
+
+    @Override
+    public boolean exists(String id, IoParameters parameters) {
+        return parameterService.exists(id, parameters);
+    }
 
     @Override
     public boolean supportsRawData() {
@@ -57,5 +98,10 @@ public class CachingDataService implements DataService<Data<AbstractValue< ? >>>
     @Override
     public RawDataService getRawDataService() {
         return null;
+    }
+
+    @Override
+    public DataCollection<Data<AbstractValue<?>>> getData(RequestParameterSet parameters) {
+        return dataService.getData(parameters);
     }
 }
---
+++ src/main/java/org/n52/cache/DataCache.java
@@ -0,0 +1,15 @@
+package org.n52.cache;
+
+import org.n52.io.request.RequestParameterSet;
+import org.n52.io.response.dataset.AbstractValue;
+import org.n52.io.response.dataset.Data;
+import org.n52.io.response.dataset.DataCollection;
+
+import java.util.Optional;
+
+public interface DataCache {
+    boolean isDataCached(RequestParameterSet parameters);
+    Optional<DataCollection<Data<AbstractValue< ? >>>> getData(RequestParameterSet parameters);
+    void putDataForParameters(RequestParameterSet parameters, DataCollection<Data<AbstractValue< ? >>> data);
+
+}
